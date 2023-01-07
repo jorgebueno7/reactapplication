93b2af18affa67f3462dc40b97ebab65096299da
thebezze <jorge7bueno@gmail.com>
2023-01-07 19:32:18 +0100

    Aplicación completa

--- client/src/routes/product/CreateProducts.jsx
+++ client/src/routes/product/CreateProducts.jsx
@@ -75,8 +75,8 @@ function CreateProducts() {
                                 <input type="text" onChange={handleChange} value={credentials.tipo} name="tipo" placeholder="Introduce el tipo del producto" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                             </div>
                             <div>
-                                <label htmlFor="double" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Precio</label>
-                                <input type="double" onChange={handleChange} value={credentials.precio} name="precio" placeholder="Introduce el precio de tu producto" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                <label htmlFor="number" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Precio</label>
+                                <input type="number" onChange={handleChange} value={credentials.precio} name="precio" placeholder="Introduce el precio de tu producto" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                             </div>
                             <button onClick={postProduct} className="w-full text-white bg-blue-600 hover:bg-primary-200 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Crear producto</button>
                         </form>
